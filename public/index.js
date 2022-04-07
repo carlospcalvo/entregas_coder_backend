05a1da8245c7043398ca0434f2b8305bdf980354
Carlos Calvo Nazabal <carloscalvonazabal@gmail.com>
2022-04-07 09:14:53 -0300

    added heroku url to frontend script

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ window.onload = async () => {
 	try {
 		const { data } = await axios({
 			method: "GET",
-			url: `http://localhost:8080/info/data`,
+			url: `https://warm-wave-22517.herokuapp.com/info/data`,
 			headers: {
 				"Content-type": "application/json;charset=utf-8",
 				"Allow-Access-Control-Origin": "*",
